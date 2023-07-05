51ab7f4596c33bf04e0c624cf9da05fdf31ec136
Mikhail Starikov <michail.a.starikov@gmail.com>
2023-07-05 18:25:06 +0200

    consult page

---
+++ precache-manifest.1187ecd9813f4f7896c55fee313f8b3c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e994feb2e9d9103d1f3",
+    "url": "/css/app.232bb556.css"
+  },
+  {
+    "revision": "3e994feb2e9d9103d1f3",
+    "url": "/js/app.e0a3d536.js"
+  },
+  {
+    "revision": "19fc4e8294f0b38f721e",
+    "url": "/js/chunk-vendors.d1a47633.js"
+  },
+  {
+    "revision": "12108809f49c49bcdc126dcecc938761",
+    "url": "/fonts/nexa_light.12108809.otf"
+  },
+  {
+    "revision": "b680a0b18a0ff3ee0180e8da7423dd6a",
+    "url": "/img/ava.b680a0b1.jpg"
+  },
+  {
+    "revision": "bbededdcd181514e046c8df0882e5421",
+    "url": "/img/mail.bbededdc.svg"
+  },
+  {
+    "revision": "18595416d120b3320257a5134c614ca0",
+    "url": "/img/medium.18595416.svg"
+  },
+  {
+    "revision": "8f92eb023034b3f603be704fc59b48d6",
+    "url": "/img/consult-ava.8f92eb02.png"
+  },
+  {
+    "revision": "89ad4412d879cfa880182efc20801da5",
+    "url": "/img/linkedin.89ad4412.svg"
+  },
+  {
+    "revision": "e9e3eb99564e85dd792b7c144a972757",
+    "url": "/img/article.e9e3eb99.png"
+  },
+  {
+    "revision": "ec8ee8e4f9f021ed1d8dad4ce4e5cdfe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "653d077300a12f09a69caeea7a8947f8",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2072840c95338bdae7295fef82567629.js"
+  "/precache-manifest.1187ecd9813f4f7896c55fee313f8b3c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "starikovm_portfolio_website"});
